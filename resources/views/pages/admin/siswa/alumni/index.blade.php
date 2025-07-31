464345d9e3f83427036329e104b491f378070ca7
sulthonyakbar <sulthonyakbar18@gmail.com>
2025-07-31 17:54:33 +0700

    minor

--- resources/views/pages/admin/siswa/alumni/index.blade.php
+++ resources/views/pages/admin/siswa/alumni/index.blade.php
@@ -9,7 +9,7 @@
 @section('main')
     <section class="section">
         <div class="section-header">
-            <h1>Data Siswa</h1>
+            <h1>Data Alumni Siswa</h1>
             <div class="section-header-breadcrumb">
                 <div class="breadcrumb-item active"><a href="{{ route('admin.dashboard') }}">Dashboard</a></div>
                 <div class="breadcrumb-item"><a href="{{ route('alumni.index') }}">Data Alumni Siswa</a></div>
